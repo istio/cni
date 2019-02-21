a4a9b826711306576cbe36dcc807417a4a6e53bd
Tim Swanson <tiswanso@cisco.com>
2019-02-21 14:01:07 -0500

    Cleanup run_e2e.sh and install istio-cni in kube-system namespace

--- .circleci/run_e2e.sh
+++ .circleci/run_e2e.sh
@@ -8,26 +8,17 @@ mkdir ${chartdir}
 
 cd /go/src/istio.io/istio
 
-HUB=${HUB:-gcr.io/istio-release}
-TAG=${TAG:-master-latest-daily}
-
+# Install istio-cni prior to executing the Istio e2e test.  Now that the helm chart for istio/istio no longer
+# depends on the istio-cni chart, we need to explicitly do these steps.
 helm init --client-only
 helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.1.0-snapshot.6/charts
 helm fetch --untar --untardir ${chartdir} istio.io/istio-cni
-
-helm template --values ${chartdir}/istio-cni/values.yaml --name=istio-cni --namespace=istio-system --set "excludeNamespaces={}" --set hub=${CNI_HUB} --set tag=${CNI_TAG} --set pullPolicy=IfNotPresent --set logLevel=${CNI_LOGLVL:-debug}  ${chartdir}/istio-cni > istio-cni_install.yaml
-
-kubectl create ns istio-system
+helm template --values ${chartdir}/istio-cni/values.yaml --name=istio-cni --namespace=kube-system --set "excludeNamespaces={}" --set hub=${CNI_HUB} --set tag=${CNI_TAG} --set pullPolicy=IfNotPresent --set logLevel=${CNI_LOGLVL:-debug}  ${chartdir}/istio-cni > istio-cni_install.yaml
 kubectl apply -f istio-cni_install.yaml
 
+HUB=${HUB:-gcr.io/istio-release}
+TAG=${TAG:-master-latest-daily}
 
 HUB=${HUB} TAG=${TAG} make istioctl
 
-# Remove any pre-existing charts
-# ...This seems to get around an issue seen with 1.1 where helm dep update fails with:
-# /go/out/linux_amd64/release/helm dep update --skip-refresh install/kubernetes/helm/istio
-#
-# Error: Unable to move current charts to tmp dir: rename /go/src/istio.io/istio/install/kubernetes/helm/istio/charts /go/src/istio.io/istio/install/kubernetes/helm/istio/tmpcharts: invalid cross-device link
-#rm -rf /go/src/istio.io/istio/install/kubernetes/helm/istio/charts
-
-HUB=${HUB} TAG=${TAG} ENABLE_ISTIO_CNI=true EXTRA_HELM_SETTINGS="--set istio-cni.excludeNamespaces={} --set istio-cni.hub=${CNI_HUB} --set istio-cni.tag=${CNI_TAG} --set istio-cni.pullPolicy=IfNotPresent --set istio-cni.logLevel=${CNI_LOGLVL:-debug}" E2E_ARGS="--kube_inject_configmap=istio-sidecar-injector ${SKIP_CLEAN:+ --skip_cleanup}" make test/local/auth/e2e_simple
+HUB=${HUB} TAG=${TAG} ENABLE_ISTIO_CNI=true E2E_ARGS="--kube_inject_configmap=istio-sidecar-injector ${SKIP_CLEAN:+ --skip_cleanup}" make test/local/auth/e2e_simple
