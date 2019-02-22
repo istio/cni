5cc7b18ec2b602a88460f910cfebbe9a9569caa0
Tim Swanson <tiswanso@cisco.com>
2019-02-21 21:37:02 -0500

    Fix comments in run_e2e.sh

--- .circleci/run_e2e.sh
+++ .circleci/run_e2e.sh
@@ -3,15 +3,14 @@
 CNI_HUB=${CNI_HUB:-istio}
 CNI_TAG=${CNI_TAG:-$1}
 
+# Dockerfile copies the PR's istio/cni helm chart into /go/helm/istio-cni
 chartdir=/go/helm
 
 cd /go/src/istio.io/istio
 
 # Install istio-cni prior to executing the Istio e2e test.  Now that the helm chart for istio/istio no longer
-# depends on the istio-cni chart, we need to explicitly do these steps.
-helm init --client-only
-#helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.1.0-snapshot.6/charts
-#helm fetch --untar --untardir ${chartdir} istio.io/istio-cni
+# depends on the istio-cni chart, we need to explicitly do this as a prereq for installing Istio
+# (the e2e_simple test installs Istio).
 helm template --values ${chartdir}/istio-cni/values.yaml --name=istio-cni --namespace=kube-system --set "excludeNamespaces={}" --set hub=${CNI_HUB} --set tag=${CNI_TAG} --set pullPolicy=IfNotPresent --set logLevel=${CNI_LOGLVL:-debug}  ${chartdir}/istio-cni > istio-cni_install.yaml
 kubectl apply -f istio-cni_install.yaml
 
