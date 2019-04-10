ef3d3280023ad7498c51c09ab7d0139aabc7912b
Tim Swanson <tiswanso@cisco.com>
2019-04-10 11:39:36 -0400

    Use private istio ipv6 images and allow disable of CNI for ipv6 test

--- .circleci/run_e2e.sh
+++ .circleci/run_e2e.sh
@@ -2,7 +2,7 @@
 
 CNI_HUB=${CNI_HUB:-istio}
 CNI_TAG=${CNI_TAG:-$1}
-ENABLE_CNI=${ENABLE_CNI:-true}
+CNI_ENABLE=${CNI_ENABLE:-true}
 
 # Dockerfile copies the PR's istio/cni helm chart into /go/helm/istio-cni
 chartdir=/go/helm
@@ -20,7 +20,7 @@ kubectl version
 echo "k8s Nodes"
 kubectl get nodes
 
-if [[ "${ENABLE_CNI}" == "true" ]]; then
+if [[ "${CNI_ENABLE}" == "true" ]]; then
     # Install istio-cni prior to executing the Istio e2e test.  Now that the helm chart for istio/istio no longer
     # depends on the istio-cni chart, we need to explicitly do this as a prereq for installing Istio
     # (the e2e_simple test installs Istio).
@@ -28,7 +28,7 @@ if [[ "${ENABLE_CNI}" == "true" ]]; then
     kubectl apply -f istio-cni_install.yaml
 fi
 
-echo "k8s: All pods (CNI enabled: ${ENABLE_CNI})"
+echo "k8s: All pods (CNI enabled: ${CNI_ENABLE})"
 kubectl get pods --all-namespaces -o wide
 
 HUB=${HUB:-gcr.io/istio-release}
@@ -36,4 +36,4 @@ TAG=${TAG:-master-latest-daily}
 
 HUB=${HUB} TAG=${TAG} make istioctl
 
-HUB=${HUB} TAG=${TAG} ENABLE_ISTIO_CNI=${ENABLE_CNI} E2E_ARGS="--kube_inject_configmap=istio-sidecar-injector ${SKIP_CLEAN:+ --skip_cleanup}" make test/local/auth/e2e_simple
+HUB=${HUB} TAG=${TAG} ENABLE_ISTIO_CNI=${CNI_ENABLE} E2E_ARGS="--kube_inject_configmap=istio-sidecar-injector ${SKIP_CLEAN:+ --skip_cleanup}" make test/local/auth/e2e_simple
