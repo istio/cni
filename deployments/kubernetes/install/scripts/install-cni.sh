ea5a40feda704b35592f12a020fb970100fb1053
Romain Lenglet <rlenglet@google.com>
2019-05-23 15:10:50 -0700

    Fix ShellCheck warnings

--- deployments/kubernetes/install/scripts/install-cni.sh
+++ deployments/kubernetes/install/scripts/install-cni.sh
@@ -12,7 +12,7 @@ set -u -e
 # Usage:
 # some_command || exit_with_error "some_command_failed: maybe try..."
 exit_with_error(){
-  echo $1
+  echo "$1"
   exit 1
 }
 
@@ -27,21 +27,21 @@ function rm_bin_files() {
 #
 function find_cni_conf_file() {
     cni_cfg=
-    for cfgf in $(ls ${MOUNTED_CNI_NET_DIR}); do
-        if [ "${cfgf: -5}" == ".conf" ]; then
+    for cfgf in "${MOUNTED_CNI_NET_DIR}"/*; do
+        if [ "${cfgf: -5}" = ".conf" ]; then
             # check if it's a valid CNI .conf file
-            type=$(cat ${MOUNTED_CNI_NET_DIR}/${cfgf} | jq 'has("type")' 2>/dev/null)
-            if [[ "${type}" == "true" ]]; then
-                cni_cfg=${cfgf}
+            type=$(jq 'has("type")' < "${cfgf}" 2>/dev/null)
+            if [ "${type}" = "true" ]; then
+                cni_cfg=$(basename "${cfgf}")
                 break
             fi
-        elif [ "${cfgf: -9}" == ".conflist" ]; then
+        elif [ "${cfgf: -9}" = ".conflist" ]; then
             # Check that the file is json and has top level "name" and "plugins" keys
             # NOTE: "cniVersion" key is not required by libcni (kubelet) to be present
-            name=$(cat ${MOUNTED_CNI_NET_DIR}/${cfgf} | jq 'has("name")' 2>/dev/null)
-            plugins=$(cat ${MOUNTED_CNI_NET_DIR}/${cfgf} | jq 'has("plugins")' 2>/dev/null)
-            if [[ "${name}" == "true" && "${plugins}" == "true" ]]; then
-                cni_cfg=${cfgf}
+            name=$(jq 'has("name")' < "${cfgf}" 2>/dev/null)
+            plugins=$(jq 'has("plugins")' < "${cfgf}" 2>/dev/null)
+            if [ "${name}" = "true" ] && [ "${plugins}" = "true" ]; then
+                cni_cfg=$(basename "${cfgf}")
                 break
             fi
         fi
@@ -51,8 +51,8 @@ function find_cni_conf_file() {
 
 function check_install() {
   cfgfile_nm=$(find_cni_conf_file)
-  if [[ "${cfgfile_nm}" != "${CNI_CONF_NAME}" ]]; then
-    if [[ "${CNI_CONF_NAME_OVERRIDE}" != "" ]]; then
+  if [ "${cfgfile_nm}" != "${CNI_CONF_NAME}" ]; then
+    if [ -n "${CNI_CONF_NAME_OVERRIDE}" ]; then
        # Install was run with overridden cni config file so don't error out on the preempt check.
        # Likely the only use for this is testing this script.
        echo "WARNING: Configured CNI config file \"${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}\" preempted by \"$cfgfile_nm\"."
@@ -62,8 +62,8 @@ function check_install() {
     fi
   fi
   if [ -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}" ]; then
-    istiocni_conf=$(cat ${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME} | jq '.plugins[]? | select(.type == "istio-cni")')
-    if [[ "$istiocni_conf" == "" ]]; then
+    istiocni_conf=$(jq '.plugins[]? | select(.type == "istio-cni")' < "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}")
+    if [ -z "$istiocni_conf" ]; then
       echo "ERROR: istio-cni CNI config removed from file: \"${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}\""
       exit 1  
     fi
@@ -78,12 +78,12 @@ function cleanup() {
 
   if [ -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}" ]; then
     echo "Removing istio-cni config from CNI chain config in ${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}"
-    CNI_CONF_DATA=$(cat ${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME} | jq 'del( .plugins[]? | select(.type == "istio-cni"))')
-    echo "${CNI_CONF_DATA}" > ${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}
+    CNI_CONF_DATA=$(jq 'del( .plugins[]? | select(.type == "istio-cni"))' < "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}")
+    echo "${CNI_CONF_DATA}" > "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}"
   fi
   if [ -e "${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}" ]; then
     echo "Removing istio-cni kubeconfig file: ${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}"
-    rm ${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}
+    rm "${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}"
   fi
   rm_bin_files
   echo "Exiting."
@@ -126,33 +126,32 @@ do
   fi
   for path in /opt/cni/bin/*;
   do
-    filename="$(basename $path)"
+    filename=$(basename "$path")
     tmp=",$filename,"
     if [ "${SKIP_CNI_BINARIES#*$tmp}" != "$SKIP_CNI_BINARIES" ];
     then
       echo "$filename is in SKIP_CNI_BINARIES, skipping"
       continue
     fi
-    if [ "${UPDATE_CNI_BINARIES}" != "true" -a -f $dir/$filename ];
+    if [ "${UPDATE_CNI_BINARIES}" != "true" ] && [ -f "$dir/$filename" ];
     then
       echo "$dir/$filename is already here and UPDATE_CNI_BINARIES isn't true, skipping"
       continue
     fi
-    cp $path $dir/ || exit_with_error "Failed to copy $path to $dir. This may be caused by selinux configuration on the host, or something else."
+    cp "$path" "$dir/" || exit_with_error "Failed to copy $path to $dir. This may be caused by selinux configuration on the host, or something else."
   done
 
-  echo "Wrote Istio CNI binaries to $dir"
-  #echo "CNI plugin version: $($dir/istio-cni -v)"
+  echo "Wrote Istio CNI binaries to $dir."
 done
 
 TMP_CONF='/istio-cni.conf.tmp'
 # If specified, overwrite the network configuration file.
-: ${CNI_NETWORK_CONFIG_FILE:=}
-: ${CNI_NETWORK_CONFIG:=}
+: "${CNI_NETWORK_CONFIG_FILE:=}"
+: "${CNI_NETWORK_CONFIG:=}"
 if [ -e "${CNI_NETWORK_CONFIG_FILE}" ]; then
   echo "Using CNI config template from ${CNI_NETWORK_CONFIG_FILE}."
   cp "${CNI_NETWORK_CONFIG_FILE}" "${TMP_CONF}"
-elif [ "${CNI_NETWORK_CONFIG}" != "" ]; then
+elif [ -n "${CNI_NETWORK_CONFIG}" ]; then
   echo "Using CNI config template from CNI_NETWORK_CONFIG environment variable."
   cat >$TMP_CONF <<EOF
 ${CNI_NETWORK_CONFIG}
@@ -164,31 +163,31 @@ SERVICE_ACCOUNT_PATH=/var/run/secrets/kubernetes.io/serviceaccount
 KUBE_CA_FILE=${KUBE_CA_FILE:-$SERVICE_ACCOUNT_PATH/ca.crt}
 SKIP_TLS_VERIFY=${SKIP_TLS_VERIFY:-false}
 # Pull out service account token.
-SERVICEACCOUNT_TOKEN=$(cat $SERVICE_ACCOUNT_PATH/token)
+SERVICEACCOUNT_TOKEN=$(cat "$SERVICE_ACCOUNT_PATH/token")
 
 # Check if we're running as a k8s pod.
 if [ -f "$SERVICE_ACCOUNT_PATH/token" ]; then
   # We're running as a k8d pod - expect some variables.
-  if [ -z ${KUBERNETES_SERVICE_HOST} ]; then
+  if [ -z "${KUBERNETES_SERVICE_HOST}" ]; then
     echo "KUBERNETES_SERVICE_HOST not set"; exit 1;
   fi
-  if [ -z ${KUBERNETES_SERVICE_PORT} ]; then
+  if [ -z "${KUBERNETES_SERVICE_PORT}" ]; then
     echo "KUBERNETES_SERVICE_PORT not set"; exit 1;
   fi
 
-  if [ "$SKIP_TLS_VERIFY" == "true" ]; then
+  if [ "$SKIP_TLS_VERIFY" = "true" ]; then
     TLS_CFG="insecure-skip-tls-verify: true"
   elif [ -f "$KUBE_CA_FILE" ]; then
-    TLS_CFG="certificate-authority-data: $(cat $KUBE_CA_FILE | base64 | tr -d '\n')"
+    TLS_CFG="certificate-authority-data: "$(base64 < "$KUBE_CA_FILE" | tr -d '\n')
   fi
 
   # Write a kubeconfig file for the CNI plugin.  Do this
   # to skip TLS verification for now.  We should eventually support
   # writing more complete kubeconfig files. This is only used
   # if the provided CNI network config references it.
-  touch ${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}
-  chmod ${KUBECONFIG_MODE:-600} ${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}
-  cat > ${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME} <<EOF
+  touch "${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}"
+  chmod "${KUBECONFIG_MODE:-600}" "${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}"
+  cat > "${MOUNTED_CNI_NET_DIR}/${KUBECFG_FILE_NAME}" <<EOF
 # Kubeconfig file for Istio CNI plugin.
 apiVersion: v1
 kind: Config
@@ -213,14 +212,13 @@ fi
 
 
 # Insert any of the supported "auto" parameters.
-grep "__KUBERNETES_SERVICE_HOST__" $TMP_CONF && sed -i s/__KUBERNETES_SERVICE_HOST__/${KUBERNETES_SERVICE_HOST}/g $TMP_CONF
-grep "__KUBERNETES_SERVICE_PORT__" $TMP_CONF && sed -i s/__KUBERNETES_SERVICE_PORT__/${KUBERNETES_SERVICE_PORT}/g $TMP_CONF
-sed -i s/__KUBERNETES_NODE_NAME__/${KUBERNETES_NODE_NAME:-$(hostname)}/g $TMP_CONF
-sed -i s/__KUBECONFIG_FILENAME__/${KUBECFG_FILE_NAME}/g $TMP_CONF
-
-# Use alternative command character "~", since these include a "/".
-sed -i s~__KUBECONFIG_FILEPATH__~${HOST_CNI_NET_DIR}/${KUBECFG_FILE_NAME}~g $TMP_CONF
-sed -i s~__LOG_LEVEL__~${LOG_LEVEL:-warn}~g $TMP_CONF
+sed -e "s/__KUBERNETES_SERVICE_HOST__/${KUBERNETES_SERVICE_HOST}/g" \
+    -e "s/__KUBERNETES_SERVICE_PORT__/${KUBERNETES_SERVICE_PORT}/g" \
+    -e "s/__KUBERNETES_NODE_NAME__/${KUBERNETES_NODE_NAME:-$(hostname)}/g" \
+    -e "s/__KUBECONFIG_FILENAME__/${KUBECFG_FILE_NAME}/g" \
+    -e "s~__KUBECONFIG_FILEPATH__~${HOST_CNI_NET_DIR}/${KUBECFG_FILE_NAME}~g" \
+    -e "s~__LOG_LEVEL__~${LOG_LEVEL:-warn}~g" \
+    -i $TMP_CONF
 
 CNI_OLD_CONF_NAME=${CNI_OLD_CONF_NAME:-${CNI_CONF_NAME}}
 
@@ -228,25 +226,22 @@ CNI_OLD_CONF_NAME=${CNI_OLD_CONF_NAME:-${CNI_CONF_NAME}}
 # This way auth token is not visible in the logs.
 echo "CNI config: $(cat ${TMP_CONF})"
 
-sed -i s/__SERVICEACCOUNT_TOKEN__/${SERVICEACCOUNT_TOKEN:-}/g $TMP_CONF
+sed -e "s/__SERVICEACCOUNT_TOKEN__/${SERVICEACCOUNT_TOKEN:-}/g" -i $TMP_CONF
 
-CNI_CONF_FILE=${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}
-
-if [ ! -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}" ] && [ "${CNI_CONF_NAME: -5}" == ".conf" ] && [ -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}list" ]; then
+if [ ! -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}" ] && [ "${CNI_CONF_NAME: -5}" = ".conf" ] && [ -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}list" ]; then
     echo "${CNI_CONF_NAME} doesn't exist, but ${CNI_CONF_NAME}list does; Using it instead."
     CNI_CONF_NAME="${CNI_CONF_NAME}list"
-	CNI_CONF_FILE=${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}
 fi
 
 if [ -e "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}" ]; then
     # This section overwrites an existing plugins list entry to for istio-cni
     CNI_TMP_CONF_DATA=$(cat ${TMP_CONF})
-    CNI_CONF_DATA=$(cat ${CNI_CONF_FILE} | jq --argjson CNI_TMP_CONF_DATA "$CNI_TMP_CONF_DATA" -f /filter.jq)
+    CNI_CONF_DATA=$(jq --argjson CNI_TMP_CONF_DATA "$CNI_TMP_CONF_DATA" -f /filter.jq < "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}")
     echo "${CNI_CONF_DATA}" > ${TMP_CONF}
 fi
 
 # If the old config filename ends with .conf, rename it to .conflist, because it has changed to be a list
-if [ "${CNI_CONF_NAME: -5}" == ".conf" ]; then
+if [ "${CNI_CONF_NAME: -5}" = ".conf" ]; then
     echo "Renaming ${CNI_CONF_NAME} extension to .conflist"
     CNI_CONF_NAME="${CNI_CONF_NAME}list"
 fi
@@ -257,7 +252,7 @@ if [ "${CNI_CONF_NAME}" != "${CNI_OLD_CONF_NAME}" ]; then
 fi
 
 # Move the temporary CNI config into place.
-mv $TMP_CONF ${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME} || \
+mv $TMP_CONF "${MOUNTED_CNI_NET_DIR}/${CNI_CONF_NAME}" || \
   exit_with_error "Failed to mv files. This may be caused by selinux configuration on the host, or something else."
 
 echo "Created CNI config ${CNI_CONF_NAME}"
@@ -266,7 +261,7 @@ echo "Created CNI config ${CNI_CONF_NAME}"
 # This prevents Kubernetes from restarting the pod repeatedly.
 should_sleep=${SLEEP:-"true"}
 echo "Done configuring CNI.  Sleep=$should_sleep"
-while [ "$should_sleep" == "true"  ]; do
-  sleep $CFGCHECK_INTERVAL
+while [ "$should_sleep" = "true" ]; do
+  sleep "$CFGCHECK_INTERVAL"
   check_install
 done
